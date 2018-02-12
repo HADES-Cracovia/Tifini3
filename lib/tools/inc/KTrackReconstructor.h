ade42759893941554f561a737ffe873a96aaade1
Rafal Lalik <rafal.lalik@ph.tum.de>
2018-02-12 16:12:52 +0100

    Api from new Hydra

--- lib/tools/inc/KTrackReconstructor.h
+++ lib/tools/inc/KTrackReconstructor.h
@@ -35,6 +35,7 @@
 #include "hparticlepair.h"
 #include "hparticleevtinfo.h"
 #include "hfwdetcand.h"
+#include "hvirtualcand.h"
 #endif
 
 #include "KTifiniAnalysis.h"
@@ -44,6 +45,7 @@ class KTrackReconstructor : public KTrack
 {
 public:
     KTrackReconstructor();
+    KTrackReconstructor(const KTrackReconstructor & track);
     virtual ~KTrackReconstructor();
     void useAnglesDeg(bool use_deg);
 
@@ -51,12 +53,21 @@ public:
     KTrackReconstructor & operator=(const KTrack & cand);
     KTrackReconstructor & operator=(const TLorentzVector & cand);
 
-    void reconstruct(const HParticleCand & track_a, const HParticleCand & track_b);
-    void reconstruct(const HParticleCand & track_a, const TLorentzVector & track_b);
+    void reconstruct(KTrackReconstructor & track_a, KTrackReconstructor & track_b);
+    void reconstruct(const KTrackReconstructor & track_a, const TLorentzVector & track_b);
     void reconstruct(const TLorentzVector & track_a, const TLorentzVector & track_b);
+#ifdef HYDRA1COMP
+    void reconstruct(const HParticleCand & track_a, const TLorentzVector & track_b);
+    void reconstruct(const HParticleCand & track_a, const HParticleCand & track_b);
+    void reconstruct(KTrackReconstructor & track_a, const HParticleCand & track_b);
+#else
+    void reconstruct(const HVirtualCand & track_a, const TLorentzVector & track_b);
+    void reconstruct(const HVirtualCand & track_a, const HVirtualCand & track_b);
+    void reconstruct(KTrackReconstructor & track_a, const HVirtualCand & track_b);
+#endif
+
 #ifndef HYDRA1COMP
-    void reconstruct(const HParticleCand & track_a, const HFwDetCand & track_b);
-    void reconstruct(const HFwDetCand & track_a, const HFwDetCand & track_b);
+    HParticlePair * getPair() { return &pair_had; }
 #endif
 
     HGeomVector getDecayVertex() const { return decay_vertex; }
@@ -82,10 +93,12 @@ private:
     TTree * tree;
     TString unique_name;
 
-    Int_t full_case;            // 0=L&L, 1=l&H, 2=H&H, 11=F&L, 12=F&H L: TLorentzVector, H: HParticleCand, F: HFeDwtCand
-    HParticleCand tr_ah, tr_bh;
-#ifndef HYDRA1COMP
-    HFwDetCand tr_af, tr_bf;
+    Int_t full_case[2];         // track 0 or 1, sel. 0=LorentzVec, 1=HPartCand, 2=HFwDetCand, 3=KTrackReco
+
+#ifdef HYDRA1COMP
+    HParticleCand tr_a, tr_b;
+#else
+    HVirtualCand tr_a, tr_b;
 #endif
     TLorentzVector tr_al, tr_bl;
 
@@ -101,51 +114,6 @@ private:
 
     Float_t fEa, fMa, fPa;
     Float_t fEb, fMb, fPb;
-
-//     tree->Branch("fXf",             &g_ads.fXf,             "fXf/F");
-//     tree->Branch("fXfi",            &g_ads.fXfi,            "fXfi/F");
-
-//     tree->Branch("fMinTrackDist",   &g_ads.fMTD,            "fMinTrackDist/F" );
-//     tree->Branch("fVertDistX",      &g_ads.fVertDistX,      "fVertDistX/F");
-//     tree->Branch("fPVA",            &g_ads.fPVA,            "fPVA/F");
-//     tree->Branch("fPVA_miss",       &g_ads.fPVA_miss,       "fPVA_miss/F");
-//     tree->Branch("fDVres",          &g_ads.fDVres,          "fDVres/F");
-// 
-//     tree->Branch("fMomA",           &g_ads.fMomA,           "fMomA/F");
-//     tree->Branch("fMomB",           &g_ads.fMomB,           "fMomB/F" );
-// //    tree->Branch("fMomAx",        &g_ads.fMomAx,          "fMomAx/F");
-// //    tree->Branch("fMomAy",        &g_ads.fMomAy,          "fMomAy/F");
-// //    tree->Branch("fMomAz",        &g_ads.fMomAz,          "fMomAz/F");
-// //    tree->Branch("fMomBx",        &g_ads.fMomBx,          "fMomBx/F" );
-// //    tree->Branch("fMomBy",        &g_ads.fMomBy,          "fMomBy/F" );
-// //    tree->Branch("fMomBz",        &g_ads.fMomBz,          "fMomBz/F" );
-//     tree->Branch("fMomA_cms",       &g_ads.fMomA_cms,       "fMomA_cms/F");
-//     tree->Branch("fMomB_cms",       &g_ads.fMomB_cms,       "fMomB_cms/F");
-//     tree->Branch("fAngleAB",        &g_ads.fAngleAB,        "fAngleAB/F");
-//     tree->Branch("fRelAngleA",      &g_ads.fRelAngleA,      "fRelAngleA/F");
-//     tree->Branch("fRelAngleB",      &g_ads.fRelAngleB,      "fRelAngleB/F");
-// 
-//     tree->Branch("fMDCdEdxA",       &g_ads.fMDCdEdxA,       "fMDCdEdxA/F");
-//     tree->Branch("fMDCdEdxB",       &g_ads.fMDCdEdxB,       "fMDCdEdxB/F");
-// 
-//     tree->Branch("fChiA",           &g_ads.fChiA,           "fChiA/F");
-//     tree->Branch("fChiB",           &g_ads.fChiB,           "fChiB/F");
-//     tree->Branch("fMetaMatchQA",    &g_ads.fMetaMatchQA,    "fMetaMatchQA/F");
-//     tree->Branch("fMetaMatchQB",    &g_ads.fMetaMatchQB,    "fMetaMatchQB/F");
-//     tree->Branch("fVertDistA",      &g_ads.fVertDistA,      "fVertDistA/F");
-//     tree->Branch("fVertDistB",      &g_ads.fVertDistB,      "fVertDistB/F");
-//     tree->Branch("fHadesTracksNum", &g_ads.fHadesTracksNum, "fHadesTracksNum/I");
-//     tree->Branch("fFwDetTracksNum", &g_ads.fFwDetTracksNum, "fFwDetTracksNum/I");
-//     tree->Branch("fIsFwDetData",    &g_ads.fIsFwDetData,    "fIsFwDetData/I");
-// 
-//     tree->Branch("fMultA",          &g_ads.fMultA,          "fMultA/I");
-//     tree->Branch("fMultB",          &g_ads.fMultB,          "fMultB/I");
-// 
-//     tree->Branch("fAccA",           &g_ads.fAccA,           "fAccA/I");
-//     tree->Branch("fAccB",           &g_ads.fAccB,           "fAccB/I");
-// 
-//     tree->Branch("fBestTry",        &g_ads.fBestTry,        "fBestTry/I");
-//     tree->Branch("fEventLCounter",  &g_ads.fEventLCounter,  "fEventLCounter/I");
 };
 
 #endif // KTRACKRECONSTRUCTOR_H
